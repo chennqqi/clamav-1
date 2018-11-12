01b1270e69cc37c578ff52a09a8e93919d23ee12
chennqqi <chennqqi@qq.com>
2018-11-12 14:00:51 +0800

    add build script

---
+++ builddocker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#rm phpdecoded
+#go build -v
+go build -o avscan -ldflags "-X main.Version=$(cat VERSION) -X main.BuildTime=$(date -u +%Y%m%d)" -v 
+sudo docker build -t "sort/avscan:$(cat VERSION)" -f Dockerfile.local .
+
