6e712e40abcd29adf581109e9afe1dab1a7d9894
chennqqi <chennqqi@qq.com>
2018-11-28 18:39:20 +0800

    queud job

--- web.go
+++ web.go
@@ -13,61 +13,215 @@ import (
 	"strings"
 	"time"
 
+	utime "github.com/chennqqi/goutils/time"
+
+	"github.com/chennqqi/goutils/persistlist"
 	"github.com/gin-gonic/gin"
 	"github.com/malice-plugins/go-plugin-utils/utils"
 )
 
 type Web struct {
-	fileto   time.Duration
-	zipto    time.Duration
+	fileto utime.Duration
+	zipto  utime.Duration
+
 	callback string
-	clav     *ClamAV
+	to       utime.Duration
+
+	tmpDir   string
+	indexDir string
+
+	scanQuit chan struct{}
+	server   *http.Server
+	cancel   context.CancelFunc
+	list     persistlist.PersistList
+
+	clav *ClamAV
+}
+
+type Job struct {
+	Cb   string         `json:"cb"`
+	Dir  string         `json:"dir"`
+	Name string         `json:"name"`
+	Tid  string         `json:"tid"`
+	To   utime.Duration `json:"to"`
+}
+
+func NewWeb(dataDir, indexDir string) (*Web, error) {
+	var web Web
+
+	list, err := persistlist.NewNodbList(indexDir, PERSIST_LISTKEY_NAME)
+	if err != nil {
+		return nil, err
+	}
+
+	err = os.MkdirAll(dataDir, 0755)
+	if !os.IsExist(err) && err != nil {
+		fmt.Printf("mkdir tmp dir error: \n", err)
+		return nil, err
+	}
+
+	web.tmpDir = dataDir
+	web.list = list
+	return &web, nil
+}
+
+func (s *Web) Shutdown(ctx context.Context) error {
+	err := s.server.Shutdown(ctx)
+	s.cancel()
+	<-s.scanQuit
+	return err
+}
+
+func (s *Web) scanRoute(ctx context.Context) {
+	ticker := time.NewTicker(time.Second / 2)
+	defer ticker.Stop()
+	list := s.list
+
+__FOR_LOOP:
+	for {
+		select {
+		case <-ticker.C:
+			for {
+				var j Job
+				err := list.Pop(&j)
+				if err == persistlist.ErrNil {
+					break
+				}
+				if err != nil {
+					fmt.Println("[scanRoute] POP ERROR:", err)
+					continue
+				}
+				r, _ := s.scanDir(tmpDir, to)
+				r1 := strings.Replace(r, f.Name(), upf.Filename, -1)
+				s.doCallback(j.Cb, r1)
+				os.RemoveAll(j.Dir)
+			}
+		case <-ctx.Done():
+			break __FOR_LOOP
+		}
+	}
+	close(s.scanQuit)
 }
 
 func (s *Web) version(c *gin.Context) {
-	txt, _ := ioutil.ReadFile("/opt/hmb/VERSION")
+	txt, _ := ioutil.ReadFile("/malware/VERSION")
 	c.Data(200, "", txt)
 }
 
 func (s *Web) scanFile(c *gin.Context) {
 	var err error
-	to := s.zipto
+	to := s.fileto
 	timeout, ok := c.GetQuery("timeout")
 	if ok {
-		to, err = time.ParseDuration(timeout)
-		if err != nil {
-			to = s.fileto
+		tto, err = time.ParseDuration(timeout)
+		if err == nil {
+			to = utime.Duration(s.fileto)
 		}
 	}
 
 	upf, err := c.FormFile("filename")
 	if err != nil {
-
+		c.JSON(400,
+			CR{
+				Message: fmt.Sprintf("get form err: %s", err.Error()),
+				Status:  1,
+			})
+		return
 	}
 	src, err := upf.Open()
 	if err != nil {
-		c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+		c.JSON(400,
+			CR{
+				Message: fmt.Sprintf("open form err: %s", err.Error()),
+				Status:  1,
+			})
 		return
 	}
 	defer src.Close()
-	tmpDir, err := ioutil.TempDir("/dev/shm", "file")
-	defer os.Remove(tmpDir)
+	tmpDir, err := ioutil.TempDir(s.tmpDir, "file")
 	if err != nil {
+		c.JSON(500,
+			CR{
+				Message: fmt.Sprintf("net tmp direrr: %s", err.Error()),
+				Status:  1,
+			})
+		return
 	}
 	f, err := ioutil.TempFile(tmpDir, "scan_")
 	if err != nil {
-		c.String(http.StatusBadRequest, fmt.Sprintf("get form err: %s", err.Error()))
+		c.JSON(400,
+			CR{
+				Message: fmt.Sprintf("create temp file err: %s", err.Error()),
+				Status:  1,
+			})
 		return
 	}
-	defer os.Remove(f.Name())
 	io.Copy(f, src)
 	f.Close()
 
-	r, _ := s.scanDir(tmpDir, to)
-	c.Header("Content-type", "application/json")
-	r1 := strings.Replace(r, f.Name(), upf.Filename, -1)
-	s.doCallback(c, r1)
-	c.String(200, r1)
+	cb, exist := c.GetQuery("callback")
+	if exist {
+		list := s.list
+		var j Job
+		j.Cb = cb
+		j.Dir = tmpDir
+		j.Name = upf.Filename
+		j.Tid = 0
+		j.To = to
+		pending, err := list.Push(&j)
+		if err != nil {
+			c.JSON(400,
+				CR{
+					Message: fmt.Sprintf("add to task list err: %s", err.Error()),
+					Status:  1,
+				})
+			return
+		} else {
+			c.JSON(200,
+				CR{
+					Message: fmt.Sprintf("pending: %d", pending),
+					Status:  0,
+				})
+			return
+		}
+	} else {
+		defer os.Remove(tmpDir)
+		defer os.Remove(f.Name())
+
+		r, _ := s.scanDir(tmpDir, to)
+		c.Header("Content-type", "application/json")
+		r1 := strings.Replace(r, f.Name(), upf.Filename, -1)
+		s.doCallback(c, r1)
+		c.String(200, r1)
+	}
+}
+
+func (s *Web) flush(c *gin.Context) {
+	list := s.list
+	var count int
+	var ret struct {
+		CR
+		Count int `json:"count"`
+	}
+
+	for {
+		var task Job
+		err := list.Pop(&task)
+		if err == persistlist.ErrNil {
+			break
+		} else if err != nil {
+			ret.Message = err.Error()
+			ret.Status = 1
+			ret.Count = count
+			c.JSON(500, &ret)
+		}
+		os.RemoveAll(task.Dir)
+		count++
+	}
+	ret.Message = "OK"
+	ret.Status = 0
+	ret.Count = count
+	c.JSON(200, &ret)
 }
 
 func Unzip(src, dest string) error {
@@ -130,12 +284,25 @@ func Unzip(src, dest string) error {
 }
 
 func (s *Web) Run(port int) {
+	scanctx, cancel := context.WithCancel(context.Background())
+	s.cancel = cancel
+	s.scanQuit = make(chan struct{})
+	go s.scanRoute(scanctx)
+
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.POST("/zip", s.scanZip)
 	r.POST("/file", s.scanFile)
+	r.GET("/queued", s.queued)
+	r.POST("/flush", s.flush)
 	r.GET("/version", s.scanFile)
-	r.Run(fmt.Sprintf(":%d", port))
+
+	httpServer := &http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: r,
+	}
+	s.server = httpServer
+	return httpServer.ListenAndServe()
 }
 
 func (s *Web) scanZip(c *gin.Context) {
@@ -160,7 +327,7 @@ func (s *Web) scanZip(c *gin.Context) {
 		return
 	}
 	defer src.Close()
-	f, err := ioutil.TempFile("/dev/shm", "zip_")
+	f, err := ioutil.TempFile(s.tmpDir, "zip_")
 	if err != nil {
 		c.String(http.StatusBadRequest, fmt.Sprintf("new tmp file err: %s", err.Error()))
 		return
@@ -169,7 +336,7 @@ func (s *Web) scanZip(c *gin.Context) {
 	io.Copy(f, src)
 	f.Close()
 
-	tmpDir, err := ioutil.TempDir("/dev/shm", "scan_")
+	tmpDir, err := ioutil.TempDir(s.tmpDir, "scan_")
 	if err != nil {
 		c.String(http.StatusInternalServerError,
 			fmt.Sprintf("save zip file err: %s", err.Error()))
@@ -190,6 +357,19 @@ func (s *Web) scanZip(c *gin.Context) {
 	r1 := strings.Replace(r, tmpDir, "", -1)
 	s.doCallback(c, r1)
 	c.String(200, r1)
+}
+
+func (s *Web) doCallback(cb string, r string) {
+	go func(r, cb string) {
+		body := strings.NewReader(r)
+		resp, err := http.Post(cb, "application/json", body)
+		if err != nil {
+			fmt.Printf("do callback(%v) error: %v\n", cb, err)
+			return
+		}
+		io.Copy(ioutil.Discard, resp.Body)
+		resp.Body.Close()
+	}(r, callback)
 }
 
 func (s *Web) doCallback(c *gin.Context, r string) {
--- webcmd.go
+++ webcmd.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"time"
 
+	"github.com/chennqqi/goutils/closeevent"
+	unet "github.com/chennqqi/goutils/net"
+	utime "github.com/chennqqi/goutils/time"
 	"github.com/google/subcommands"
 )
 
@@ -13,6 +16,15 @@ type webCmd struct {
 	port     int
 	zipto    string
 	callback string
+
+	w        *Web
+	port     int
+	fileto   string
+	zipto    string
+	callback string
+	datadir  string
+	indexdir string
+	dns      string
 }
 
 func (p *webCmd) Name() string {
@@ -30,23 +42,52 @@ func (p *webCmd) Usage() string {
 func (p *webCmd) SetFlags(f *flag.FlagSet) {
 	f.IntVar(&p.port, "p", 8080, "set port")
 	f.StringVar(&p.zipto, "timeout", "60s", "set scan timeout")
-	f.StringVar(&p.zipto, "callback", "", "set callback")
+	f.StringVar(&p.fileto, "fileto", "20s", "set scan file timeout")
+	f.StringVar(&p.datadir, "data", "/dev/shm", "set data dir")
+	f.StringVar(&p.indexdir, "index", "/dev/shm/.persist", "set index dir")
+	f.StringVar(&p.dns, "nameserver", "", "set ns server, can be list split by coma")
 }
 
 func (p *webCmd) Execute(context.Context, *flag.FlagSet, ...interface{}) subcommands.ExitStatus {
-	to, err := time.ParseDuration(p.zipto)
+	zipTo, err := time.ParseDuration(p.zipto)
 	if err != nil {
-		to, _ = time.ParseDuration("60s")
+		zipTo, _ = time.ParseDuration("60s")
 	}
+	fileTo, err := time.ParseDuration(p.fileto)
+	if err != nil {
+		fileTo, _ = time.ParseDuration("20s")
+	}
+
 	if p.callback == "" {
 		p.callback = os.Getenv("HMBD_CALLBACK")
 	}
 
+	if p.dns != "" {
+		dns := strings.Split(p.dns, ",")
+		if len(dns) > 0 {
+			net.DefaultResolver = unet.NewResolver(dns)
+		}
+	} else {
+		dns := os.Getenv("HMBD_DNS")
+		dnslist := strings.Split(p.dns, ",")
+		if dns != "" && len(dnslist) > 0 {
+			net.DefaultResolver = unet.NewResolver(dnslist)
+		}
+	}
+
 	var w Web
 	w.clav, _ = NewClamAV("", false)
-	w.fileto = to
-	w.zipto = to
+	w.fileto = utime.Duration(to)
+	w.zipto = utime.Duration(to)
 	w.callback = p.callback
-	w.Run(p.port)
+	ctx, cancel := context.WithCancel(context.Background())
+	go w.Run(p.port, ctx)
+
+	closeevent.Wait(func(s os.Signal) {
+		defer cancel()
+		ctx := context.Background()
+		w.Shutdown(ctx)
+	}, os.Interrupt)
+
 	return subcommands.ExitSuccess
 }
